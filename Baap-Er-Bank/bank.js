4e59ee99cd4ee820c7cc14d0dd47bc30dc1c2ff3
ranzu02 <ranzu84@gmail.com>
2024-01-29 01:05:02 +0600

    Bank

---
+++ Baap-Er-Bank/bank.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', function(){
+    const emailField = document.getElementById('email');
+    const email = emailField.value;
+
+    const passwordField = document.getElementById('password');
+    const password = passwordField.value;
+
+    if (email === "ranzu@ranzu.com" && password === 'secret'){
+        window.location.href ='bank.html';
+    }
+    else {
+        alert("It's not a valid user");
+    }
+})
