bcc556ed3dc9c6807571f461415d67ceb28f8534
ranzu02 <ranzu84@gmail.com>
2024-02-15 01:47:46 +0600

    learning

--- Summer_Sale_Assignment/js/script.js
+++ Summer_Sale_Assignment/js/script.js
@@ -27,6 +27,45 @@ function getProductPrice(price, productName){
     if (priceAfterAddingItemTwo >= 200){
         const button = document.getElementById('button-apply');
         button.removeAttribute('disabled');
+        
+    }
+
+    if (priceAfterAddingItemTwo > 0){
+        const purchaseButton = document.getElementById('purchase-button');
+        purchaseButton.removeAttribute('disabled');
+    }
+
+    
+}
+
+
+const applyButton = document.getElementById('button-apply');
+applyButton.addEventListener('click', applyDiscount);
+
+function applyDiscount(){
+    console.log('apply button clicked');
+    const couponElement = document.getElementById('coupon-element');
+    const couponValue = couponElement.value;
+    if(couponValue === 'SELL200') {
+        const totalPriceElement = document.getElementById('total-price');
+        const totalPriceString = totalPriceElement.innerText;
+        const totalPrice = parseFloat(totalPriceString);
+        
+        const discount = (totalPrice * 20) / 100;
+        const discountFixed = discount.toFixed(2);
+        const discountElement = document.getElementById('discount');
+        discountElement.innerText = discountFixed;
+        
+        const finalPrice = totalPrice - discount;
+        const finalPriceFixed =finalPrice.toFixed(2);
+        const totalElement = document.getElementById('price-after-discount');
+        totalElement.innerText = finalPriceFixed;
+        
+
+    }
+
+    else {
+        alert('Please enter correct Coupond Code');
     }
 }
 
